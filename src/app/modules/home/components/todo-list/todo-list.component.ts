ea8f2771a0cc14c1902944cb2224055fca6b369a
Daniel <danielbou@outlook.com.br>
2022-04-03 21:05:16 -0300

    Iterando as tasks na lista

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,17 @@
+import { TaskList } from './../../model/task-list';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova task', checked: true },
+    { task: 'Minha nova task 2', checked: false },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
